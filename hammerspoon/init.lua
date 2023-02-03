8effbda6ad93b98aa03344eac94c916f8e8ed5c0
Ian Chen <ianchen06@gmail.com>
2023-02-03 18:50:45 +0900

    Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -40,3 +40,28 @@ end
 other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
 other_tap:start()
 
+local simpleCmd = false
+local map = hs.keycodes.map
+local function eikanaEvent(event)
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    if event:getType() == hs.eventtap.event.types.keyDown then
+        if f['cmd'] then
+            simpleCmd = true
+        end
+    elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+        if not f['cmd'] then
+            if simpleCmd == false then
+                if c == map['cmd'] then
+                    hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+                elseif c == map['rightcmd'] then
+                    hs.keycodes.setMethod('Pinyin - Traditional')
+                end
+            end
+            simpleCmd = false
+        end
+    end
+end
+
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana:start()
